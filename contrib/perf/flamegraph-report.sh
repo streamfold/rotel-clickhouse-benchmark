5baf83c01be7b4c7f9a653cf26850c09359a5414
Mike Heffner <mikeh@fesnel.com>
2025-10-31 13:47:30 -0400

    install perl-open

--- contrib/perf/flamegraph-report.sh
+++ contrib/perf/flamegraph-report.sh
@@ -5,6 +5,8 @@ if [ ! -d $HOME/flamegraph ]; then
     cd $HOME
     git clone https://github.com/brendangregg/FlameGraph.git flamegraph
     cd -
+    
+    sudo dnf install -y perl-open
 fi
 
 if [ ! -e "perf.data" ]; then
