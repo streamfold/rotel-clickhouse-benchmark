65237e8b1a07d7c1b8cbaf695802861ba5d38e6b
Mike Heffner <mikeh@fesnel.com>
2025-11-08 10:37:26 -0500

    switch to 0.0.0.0

--- contrib/rotel/run-rotel-edge.sh
+++ contrib/rotel/run-rotel-edge.sh
@@ -7,6 +7,8 @@ if [ -z "$ROTEL_BIN" ]; then
 fi
 
 exec $ROTEL_BIN start \
+    --otlp-grpc-endpoint 0.0.0.0:4317 \
+    --otlp-http-endpoint 0.0.0.0:4318 \
     --exporter=kafka \
     --kafka-exporter-brokers ${KAFKA_HOST:-kafka}:9092 \
     --kafka-exporter-max-message-bytes 5242880 \
